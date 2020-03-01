01ab4df8d6b123691121e2c2c66e30d432e92af1
inTEGr8or <admin@thefullertonian.com>
2020-02-29 20:33:10 -0800

    updates

--- deploy.ps1
+++ deploy.ps1
@@ -2,5 +2,5 @@
 git pull upstream master
 py .\coronavirus2019.py
 hugo
-Set-Content -Path 'docs/CNAME' -Value 'statis.global'
+Set-Content -Path 'docs/CNAME' -Value 'stasis.global'
 git add .; git commit -m 'updates'; git push

3a54cfcc399a2e20fd6e02040897789f13fd6e0e
inTEGr8or <admin@thefullertonian.com>
2020-03-01 08:38:22 -0800

    updates

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,12 @@
 # build and deploy the site
-git pull upstream master
-py .\coronavirus2019.py
-hugo
-Set-Content -Path 'docs/CNAME' -Value 'stasis.global'
-git add .; git commit -m 'updates'; git push
+param(
+    [boolean] $Force
+)
+$pull_result = git pull upstream master
+echo $pull_result
+if ($pull_result -ne "Already up to date." -or $Force){
+    py .\coronavirus2019.py
+    hugo
+    Set-Content -Path 'docs/CNAME' -Value 'stasis.global'
+    git add .; git commit -m 'updates'; git push
+}

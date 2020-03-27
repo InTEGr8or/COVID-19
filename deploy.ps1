2731d37105c1cf48e53521656644948221871c86
inTEGr8or <admin@thefullertonian.com>
2020-03-26 21:33:59 -0700

    updates

--- deploy.ps1
+++ deploy.ps1
@@ -7,6 +7,7 @@ Write-Output $pull_result
 if ($pull_result -ne "Already up to date." -or $Force){
     py .\coronavirus2019.py
     pageres 'https://covid19info.live/' 1180x1500 --selector='#statistics > div.map-container' --filename='content/covidinfo.live.png' --crop --overwrite
+    pageres 'https://www.worldometers.info/coronavirus/' 1180x1500 --selector='body > div.container > div:nth-child(2) > div.col-md-8 > div > div:nth-child(14)' --filename='content/worldometers_coronavirus.png' --crop --overwrite
     # pageres 'https://covid19info.live/' 1180x1500 --selector='#statistics > div.map-container' --filename='content/covidinfo.live.png' --crop --overwrite
     hugo
     Set-Content -Path 'docs/CNAME' -Value 'stasis.global'
